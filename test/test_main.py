9ada349f0814b64a5a1cb9a436c00e454685b4ea
AlbinLind <albin.lindqvist@hotmail.se>
2024-02-20 16:49:26 +0100

    Updates query format

--- test/test_main.py
+++ test/test_main.py
@@ -2,27 +2,31 @@ from pydantic import ValidationError
 import pytest
 from database.main import Table, remove_database, User, Engine
 
+
 def test_create_table():
     class A(Table):
         id: int
         something: str
 
-    _ = A(id=1, something="hello")        
-    
+    _ = A(id=1, something="hello")
+
     with pytest.raises(ValidationError):
         _ = A()
 
     _ = A(id="1", something="sdfo")
 
+
 def test_add_database():
     engine = Engine(":memory:")
     engine.conn.execute("SELECT * FROM sqlite_master")
+
 
 def test_add_table():
     engine = Engine(":memory:")
     engine.push(User)
     res = engine.conn.execute("SELECT * FROM sqlite_master").fetchone()
     assert res is not None
+
 
 def test_add_single_row():
     engine = Engine(":memory:")
@@ -32,6 +36,7 @@ def test_add_single_row():
     res = engine.conn.execute("SELECT count(*) FROM User").fetchone()
     assert res[0] == 1
 
+
 def test_add_multiple_rows():
     engine = Engine(":memory:")
     engine.push(User)
@@ -40,22 +45,25 @@ def test_add_multiple_rows():
     engine.insert([usr, usr2])
     res = engine.conn.execute("SELECT count(*) FROM User").fetchone()
     assert res[0] == 2
-    
+
+
 def test_inserting_returns_same_object():
     engine = Engine(":memory:")
     engine.push(User)
     usr = User(name="Hello")
     res = engine.insert([usr])
     assert id(res[0]) == id(usr)
-    
+
+
 def test_query_single_row():
     engine = Engine(":memory:")
     engine.push(User)
     usr = User(name="Hello")
     engine.insert(usr)
-    res = engine.query(User)
+    res = engine.query(User).all()
     assert len(res) == 1
     assert isinstance(res[0], User)
+
 
 def test_query_with_limit():
     engine = Engine(":memory:")
@@ -63,10 +71,11 @@ def test_query_with_limit():
     usr = User(name="Hello")
     usr2 = User(name="Name number 2")
     engine.insert([usr, usr2])
-    res = engine.query(User).limit(1)
+    res = engine.query(User).limit(1).all()
     assert len(res) == 1
     assert isinstance(res[0], User)
     assert res[0].name == usr.name
+
 
 def test_query_with_offset():
     engine = Engine(":memory:")
@@ -74,10 +83,11 @@ def test_query_with_offset():
     usr = User(name="Hello")
     usr2 = User(name="Name number 2")
     engine.insert([usr, usr2])
-    res = engine.query(User).offset(1)
+    res = engine.query(User).offset(1).all()
     assert len(res) == 1
     assert isinstance(res[0], User)
     assert res[0].name == usr2.name
+
 
 def test_query_with_limit_and_offset():
     engine = Engine(":memory:")
@@ -86,10 +96,11 @@ def test_query_with_limit_and_offset():
     usr2 = User(name="Name number 2")
     usr3 = User(name="Name number 3")
     engine.insert([usr, usr2, usr3])
-    res = engine.query(User).limit(1).offset(1)
+    res = engine.query(User).limit(1).offset(1).all()
     assert len(res) == 1
     assert isinstance(res[0], User)
     assert res[0].name == usr2.name
+
 
 def test_query_with_where():
     engine = Engine(":memory:")
@@ -98,10 +109,11 @@ def test_query_with_where():
     usr2 = User(name="Name number 2")
     usr3 = User(name="Name number 3")
     engine.insert([usr, usr2, usr3])
-    res = engine.query(User).where(User.c.name == "Name number 2")
+    res = engine.query(User).where(User.c.name == "Name number 2").all()
     assert len(res) == 1
     assert isinstance(res[0], User)
     assert res[0].name == usr2.name
+
 
 def test_query_with_multiple_where():
     engine = Engine(":memory:")
@@ -110,7 +122,11 @@ def test_query_with_multiple_where():
     usr2 = User(name="Name number 2")
     usr3 = User(name="Name number 3")
     engine.insert([usr, usr2, usr3])
-    res = engine.query(User).where(User.c.name == "Name number 2" | User.c.name == "Name number 3")
+    res = (
+        engine.query(User)
+        .where(User.c.name == "Name number 2" | User.c.name == "Name number 3")
+        .all()
+    )
     assert len(res) == 2
     assert isinstance(res[0], User)
     assert res[0].name == usr2.name
