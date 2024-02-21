c9730b9a06f1821d83bf02cc332ea99e260d86fc
AlbinLind <albin.lindqvist@hotmail.se>
2024-02-21 14:11:36 +0100

    Formats code

--- database/main.py
+++ database/main.py
@@ -28,16 +28,19 @@ def _get_type(field: Any) -> str:
         return "INTEGER"
     return "BLOB"
 
+
 class QueryClause(Enum):
     SELECT = 0
     FROM = 1
     WHERE = 2
-    GROUP_BY =  3
+    GROUP_BY = 3
     ORDER_BY = 4
     LIMIT = 5
     OFFSET = 6
 
+
 C = TypeVar("C", "Column", tuple[Any])
+
 
 class Query:
     def __init__(self, select: C | "Table", engine: "Engine"):
@@ -46,49 +49,58 @@ class Query:
         self.select = select
         # We initially assume we can return an object if we are selecting from a table
         # Otherwise, we will return a tuple of size equal to the size of the select list
-        # For example, if we add a group by clause we can no longer return an object, 
+        # For example, if we add a group by clause we can no longer return an object,
         # since we cannot know how it will look, and no model is defined for that.
         self.result_column = select
-        self._can_return_table = issubclass(select, Table) # type: ignore
+        self._can_return_table = issubclass(select, Table)  # type: ignore
         if self._can_return_table:
             self._select_table()
         else:
             self._select_columns()
-        
+
     def _select_table(self):
         self.statements.append(("SELECT *", QueryClause.SELECT, None))
-        self.statements.append((f"FROM {self.select.table_name}", QueryClause.FROM, None)) # type: ignore
+        self.statements.append(
+            (f"FROM {self.select.table_name}", QueryClause.FROM, None)
+        )  # type: ignore
         return self
 
     def _select_columns(self):
-        raise NotImplementedError("You may only select all columns from a table at this moment...")
+        raise NotImplementedError(
+            "You may only select all columns from a table at this moment..."
+        )
 
     def limit(self, limit: int):
         self.statements.append(("LIMIT ?", QueryClause.LIMIT, [limit]))
-        return self 
-    
+        return self
+
     def offset(self, offset: int):
         self.statements.append(("OFFSET ?", QueryClause.OFFSET, [offset]))
         return self
-    
+
     def where(self, clause: "WhereStatement"):
-        self.statements.append((f"WHERE {clause.statement}", QueryClause.WHERE, clause.values))
+        self.statements.append(
+            (f"WHERE {clause.statement}", QueryClause.WHERE, clause.values)
+        )
         return self
-        
+
     def _validate_query(self):
         """Make sure that the query are valid, and that there are no conflicting clauses.
         The function will try and fix the query if possible.
         For example if the query contains an OFFSET clause, but no LIMIT clause, it will set the LIMIT as -1"""
         clauses = [x[1] for x in self.statements]
         if QueryClause.SELECT not in clauses:
-            raise ValueError("You must provide a SELECT clause. If this error is raised, something has "
-                             "gone wrong when creating the query, and it is an internal error.")
+            raise ValueError(
+                "You must provide a SELECT clause. If this error is raised, something has "
+                "gone wrong when creating the query, and it is an internal error."
+            )
         if QueryClause.FROM not in clauses:
-            raise ValueError("You must provide a FROM clause. If this error is raised, something has "
-                             "gone wrong when creating the query, and it is an internal error.")
+            raise ValueError(
+                "You must provide a FROM clause. If this error is raised, something has "
+                "gone wrong when creating the query, and it is an internal error."
+            )
         if QueryClause.OFFSET in clauses and QueryClause.LIMIT not in clauses:
             self.limit(-1)
-        
 
     def _build_statement(self) -> tuple[str, list[Any]]:
         statement = ""
@@ -98,26 +110,32 @@ class Query:
         self.statements.sort(key=lambda x: x[1].value)
         for clause, clause_type, value in self.statements:
             if clause_type in clauses_added:
-                raise ValueError(f"Clause {clause_type} already added, you cannot provide a clause multiple times.")
+                raise ValueError(
+                    f"Clause {clause_type} already added, you cannot provide a clause multiple times."
+                )
             statement += " " + clause
             if value:
                 values.extend(value)
             clauses_added.add(clause_type)
         return statement, values
-        
-    def all(self) -> list[C] | list["Table"]:
-            """
-            Retrieve all rows from the database table.
 
-            Returns:
-                list[tuple[Any]] | list[Table]: A list of objects representing the rows from the table, or a tuple if the return type is not a table.
-            """
-            statement, values = self._build_statement()
-            print(statement)
-            result = self.engine.conn.execute(statement, values).fetchall()
-            if self._can_return_table:
-                return [self.engine._convert_row_to_object(self.result_column, row) for row in result] # type: ignore
-            return result
+    def all(self) -> list[C] | list["Table"]:
+        """
+        Retrieve all rows from the database table.
+
+        Returns:
+            list[tuple[Any]] | list[Table]: A list of objects representing the rows from the table, or a tuple if the return type is not a table.
+        """
+        statement, values = self._build_statement()
+        print(statement)
+        result = self.engine.conn.execute(statement, values).fetchall()
+        if self._can_return_table:
+            return [
+                self.engine._convert_row_to_object(self.result_column, row)
+                for row in result
+            ]  # type: ignore
+        return result
+
 
 class Engine:
     def __init__(self, path: str):
@@ -139,7 +157,7 @@ class Engine:
                 f"{field} {_get_type(table.model_fields[field].annotation)}"
             )
             if table.model_fields[field].json_schema_extra:
-                if table.model_fields[field].json_schema_extra.get("primary_key"): # type: ignore
+                if table.model_fields[field].json_schema_extra.get("primary_key"):  # type: ignore
                     column_statement += " PRIMARY KEY"
             columns.append(column_statement)
         statement += f"({', '.join(columns)})"
@@ -150,10 +168,14 @@ class Engine:
         if not self._check_if_table_exists(table):
             self._create_table(table)
 
-    def _convert_row_to_object(self, table: Union["Table" , type["Table"]], row: tuple[Any]) -> "Table":
+    def _convert_row_to_object(
+        self, table: Union["Table", type["Table"]], row: tuple[Any]
+    ) -> "Table":
         fields = table.model_fields
         if len(row) != len(fields):
-            raise ValueError(f"Number of columns in the row ({len(row)}) does not match the number of fields in the table ({len(fields)}).")
+            raise ValueError(
+                f"Number of columns in the row ({len(row)}) does not match the number of fields in the table ({len(fields)})."
+            )
         if isinstance(table, Table):
             for i, field in enumerate(fields):
                 if getattr(table, field) != row[i]:
@@ -161,7 +183,9 @@ class Engine:
             return table
         if isinstance(table, type):
             return table(**{field: row[i] for i, field in enumerate(fields)})
-        raise ValueError(f"Table is neither a Table nor a type, it is {type(table)}, which is not supported.")
+        raise ValueError(
+            f"Table is neither a Table nor a type, it is {type(table)}, which is not supported."
+        )
 
     def query(self, select: list["Column"] | type["Table"]) -> Query:
         return Query(select, self)
@@ -175,7 +199,10 @@ class Engine:
             values = []
             for key, value in obj.model_dump().items():
                 if obj.model_fields[key].json_schema_extra:
-                    if obj.model_fields[key].json_schema_extra.get("primary_key") and not value:  # type: ignore
+                    if (
+                        obj.model_fields[key].json_schema_extra.get("primary_key")
+                        and not value
+                    ):  # type: ignore
                         continue
                 keys.append(str(key))
                 values.append(str(value))
@@ -186,6 +213,7 @@ class Engine:
         self.conn.commit()
         return row_list
 
+
 class WhereStatement:
     def __init__(self, statement: str, values: list[Any] = []):
         self.statement = f"{statement}"
@@ -195,7 +223,7 @@ class WhereStatement:
         self.statement = f"({self.statement} AND {other.statement})"
         self.values.extend(other.values)
         return self
-    
+
     def __or__(self, other: "WhereStatement"):
         self.statement = f"({self.statement} OR {other.statement})"
         self.values.extend(other.values)
@@ -212,35 +240,47 @@ class Column:
 
     def __repr__(self):
         return f"Column(type={self.field.annotation})"
-    
-    def __eq__(self, other: Any) -> WhereStatement: # type: ignore
-        if not isinstance(other, self.field.annotation): # type: ignore
-            raise ValueError(f"Column has type {self.field.annotation}, but you are comparing it to a {type(other)}")
+
+    def __eq__(self, other: Any) -> WhereStatement:  # type: ignore
+        if not isinstance(other, self.field.annotation):  # type: ignore
+            raise ValueError(
+                f"Column has type {self.field.annotation}, but you are comparing it to a {type(other)}"
+            )
         return WhereStatement(f"{self.column_name} = ?", [other])
-    
-    def __ne__(self, other: Any) -> WhereStatement: # type: ignore
-        if not isinstance(other, self.field.annotation): # type: ignore
-            raise ValueError(f"Column has type {self.field.annotation}, but you are comparing it to a {type(other)}")
+
+    def __ne__(self, other: Any) -> WhereStatement:  # type: ignore
+        if not isinstance(other, self.field.annotation):  # type: ignore
+            raise ValueError(
+                f"Column has type {self.field.annotation}, but you are comparing it to a {type(other)}"
+            )
         return WhereStatement(f"{self.column_name} != ?", [other])
-    
-    def __lt__(self, other: Any) -> WhereStatement: # type: ignore
-        if not isinstance(other, self.field.annotation): # type: ignore
-            raise ValueError(f"Column has type {self.field.annotation}, but you are comparing it to a {type(other)}")
+
+    def __lt__(self, other: Any) -> WhereStatement:  # type: ignore
+        if not isinstance(other, self.field.annotation):  # type: ignore
+            raise ValueError(
+                f"Column has type {self.field.annotation}, but you are comparing it to a {type(other)}"
+            )
         return WhereStatement(f"{self.column_name} < ?", [other])
-    
-    def __le__(self, other: Any) -> WhereStatement: # type: ignore
-        if not isinstance(other, self.field.annotation): # type: ignore
-            raise ValueError(f"Column has type {self.field.annotation}, but you are comparing it to a {type(other)}")
+
+    def __le__(self, other: Any) -> WhereStatement:  # type: ignore
+        if not isinstance(other, self.field.annotation):  # type: ignore
+            raise ValueError(
+                f"Column has type {self.field.annotation}, but you are comparing it to a {type(other)}"
+            )
         return WhereStatement(f"{self.column_name} <= ?", [other])
-    
-    def __gt__(self, other: Any) -> WhereStatement: # type: ignore
-        if not isinstance(other, self.field.annotation): # type: ignore
-            raise ValueError(f"Column has type {self.field.annotation}, but you are comparing it to a {type(other)}")
+
+    def __gt__(self, other: Any) -> WhereStatement:  # type: ignore
+        if not isinstance(other, self.field.annotation):  # type: ignore
+            raise ValueError(
+                f"Column has type {self.field.annotation}, but you are comparing it to a {type(other)}"
+            )
         return WhereStatement(f"{self.column_name} > ?", [other])
-    
-    def __ge__(self, other: Any) -> WhereStatement: # type: ignore
-        if not isinstance(other, self.field.annotation): # type: ignore
-            raise ValueError(f"Column has type {self.field.annotation}, but you are comparing it to a {type(other)}")
+
+    def __ge__(self, other: Any) -> WhereStatement:  # type: ignore
+        if not isinstance(other, self.field.annotation):  # type: ignore
+            raise ValueError(
+                f"Column has type {self.field.annotation}, but you are comparing it to a {type(other)}"
+            )
         return WhereStatement(f"{self.column_name} >= ?", [other])
 
     def __getitem__(self, other: list[Any]) -> WhereStatement:
@@ -250,8 +290,14 @@ class Column:
         # Matrix Multiplication: User.c.id @ [1,2,3]
         # Power: User.c.id ** [1,2,3]
         if not isinstance(other, list):
-            raise ValueError(f"Column has type {self.field.annotation}, but you are comparing it to a {type(other)}")
-        return WhereStatement(f"{self.column_name} IN ({', '.join(['?' for _ in range(len(other))])})", other)
+            raise ValueError(
+                f"Column has type {self.field.annotation}, but you are comparing it to a {type(other)}"
+            )
+        return WhereStatement(
+            f"{self.column_name} IN ({', '.join(['?' for _ in range(len(other))])})",
+            other,
+        )
+
 
 class GenericColumn:
     def __init__(self, table: type["Table"]):
