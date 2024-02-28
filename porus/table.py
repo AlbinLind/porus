f64797d4a1c19a579b31f1bb8a40cab95eb959e5
Albin Lindqvist <albin.lindqvist@hotmail.se>
2024-02-28 16:00:19 +0100

    Adds documentation to table module, and fixes some minor linting issues

--- porus/table.py
+++ porus/table.py
@@ -1,20 +1,48 @@
+"""Contains the custom meta class for pydantic models and the table class."""
+from typing import Any
+
 from pydantic import BaseModel
+from pydantic._internal._model_construction import ModelMetaclass
 
 from porus.column import GenericColumn
-from pydantic._internal._model_construction import ModelMetaclass
 
 
 class TableMeta(ModelMetaclass):
-    def __getattr__(self, name):
-        if name == "_" or name == "c":
+    """Meta class for the Table class. This class is used so that we can get the "column"
+    attribute from the table class, which is actually a pydantic model.
+    """
+    def __getattr__(self, name: str)-> Any:
+        """Gets a column attribute."""
+        if name in ("_", "c"):
             return GenericColumn(self)
         return super().__getattr__(name)  # type: ignore
 
 
 class Table(BaseModel, metaclass=TableMeta):
-    def __init_subclass__(cls, **kwargs):
-        setattr(cls, "table_name", cls.__name__.lower())
+    """Base class for tables in porus ORM. This class is also a pydantic model,
+    so it can be used in the same way as you would use it. However, it also contains some other
+    notable functinality.
+
+    To use it just create a class that inherits from this class, and then add the columns as class
+    variables.
+
+    >>> from porus import Table, ColumnField
+    >>> class Article(Table):
+    >>>    id: int = ColumnField(primary_key=True)
+    >>>    title: str
+    >>>    body: str
+    >>>    author: str
+    >>> article = Article(title="Test", body="Test", author="Test")
+    >>> print(article.model_dump()) # will print the model as a dictionary.
+
+    This is what makes porus, strong.
+    """
+
+    def __init_subclass__(cls) -> None:
+        """Function that runs after a subclass is created. This function sets the table_name."""
+        cls.table_name = cls.__name__.lower() # type: ignore
 
     @property
-    def table_name(cls) -> str:
-        return cls.__class__.__name__.lower()
+    def table_name(self) -> str:
+        """Get the table name."""
+        return self.__class__.__name__.lower()
