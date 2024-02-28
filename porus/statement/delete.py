ea9c15d9dde43745dafef15b572301ff0045d540
Albin Lindqvist <albin.lindqvist@hotmail.se>
2024-02-28 16:37:07 +0100

    Adds documentation for delete

--- porus/statement/delete.py
+++ porus/statement/delete.py
@@ -1,4 +1,6 @@
+"""This module contains the Delete class, which is used to create a DELETE statement."""
 from typing import TYPE_CHECKING
+
 from porus.column import Column
 from porus.errors.clause_errors import InvalidOperationError
 from porus.statement.base_statement import BaseStatement
@@ -6,32 +8,36 @@ from porus.statement.clause_enums import DeleteClause
 from porus.table import Table, TableMeta
 
 if TYPE_CHECKING:
-    from porus.engine import Engine
     from porus.column import Column
+    from porus.engine import Engine
 
 
 class Delete(BaseStatement):
+    """Delete statement, used to delete rows from a table, just as you would with an SQL DELETE."""
     def __init__(
         self,
         *,
         table_or_subquery: type["Table"],
         engine: "Engine",
-    ):
+    ) -> None:
+        """Create a new Delete object."""
         super().__init__(table_or_subquery=table_or_subquery, engine=engine)
         self._delete_table()
 
-    def _delete_table(self):
+    def _delete_table(self) -> "Delete":
+        """Create the DELETE clause."""
         if not isinstance(self.select, TableMeta):
             raise ValueError("You cannot delete from a subquery. You must provide a table.")
         self.statements.append((f"DELETE FROM {self.select.table_name}", DeleteClause.DELETE, None))
         return self
 
-    def _validate_query(self):
+    def _validate_query(self) -> None:
+        """Validates that the query is valid."""
         if DeleteClause.DELETE not in [x[1] for x in self.statements]:
             raise ValueError("You must provide a table to delete from.")
 
-    def returning(self, *columns: Column):
-        """Return the columns after the delete. If no columns are provided, returns all columns"""
+    def returning(self, *columns: Column) -> "Delete":
+        """Return the columns after the delete. If no columns are provided, returns all columns."""
         if len(columns) == 0:
             self.statements.append(("RETURNING *", DeleteClause.RETURNING, None))
             return self
@@ -48,14 +54,18 @@ class Delete(BaseStatement):
         self._can_return_table = False
         return self
 
-    def order_by(self, *columns: Column, ascending: bool = True):
+    def order_by(self, *columns: Column, ascending: bool = True) -> None: # type: ignore
+        """You cannot use ORDER BY in a DELETE statement."""
         raise InvalidOperationError("You cannot use ORDER BY in a DELETE statement.")
 
-    def limit(self, limit: int):
+    def limit(self, limit: int) -> None: # type: ignore
+        """You cannot use LIMIT in a DELETE statement."""
         raise InvalidOperationError("You cannot use LIMIT in a DELETE statement.")
 
-    def offset(self, offset: int):
+    def offset(self, offset: int) -> None: # type: ignore
+        """You cannot use OFFSET in a DELETE statement."""
         raise InvalidOperationError("You cannot use OFFSET in a DELETE statement.")
 
-    def group_by(self, *columns: Column):
+    def group_by(self, *columns: Column) -> None: # type: ignore
+        """You cannot use GROUP BY in a DELETE statement."""
         raise InvalidOperationError("You cannot use GROUP BY in a DELETE statement.")
