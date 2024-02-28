a8dde144d1fae6f3537a8d55af1d29b95b6c5958
Albin Lindqvist <albin.lindqvist@hotmail.se>
2024-02-28 16:29:26 +0100

    Adds package docstring

--- porus/__init__.py
+++ porus/__init__.py
@@ -1,3 +1,7 @@
+"""Porus is a simple ORM for Python. It is designed to be as simple and natural as possible.
+This is not a well tested library, and it is DEFINITELY not production ready. It is just a project
+for me to learn more about SQL and Python.
+"""
 from .column import ColumnField
 from .engine import Engine
 from .table import Table
