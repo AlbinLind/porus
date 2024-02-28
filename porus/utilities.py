b26cd4ab8a2fa3879af8be367ffa3e522cc35e7b
Albin Lindqvist <albin.lindqvist@hotmail.se>
2024-02-28 16:00:58 +0100

    Adds documentation to utilitites

--- porus/utilities.py
+++ porus/utilities.py
@@ -1,9 +1,11 @@
+"""Utility functions."""
 from typing import Any
 
 
 def _get_type(field: Any) -> str:
     """Returns the SQLite affinity for the given field, if it cannot be inferred,
-    it will be returned as BLOB, which will store it exactly as it is passed, i.e. it's __repr__ or __str__ function.
+    it will be returned as BLOB, which will store it exactly as it is passed, i.e. it's __repr__ or 
+    __str__ function.
     """
     if field.__name__ == "int":
         return "INTEGER"
@@ -20,7 +22,8 @@ def _convert_values(values: list[Any]) -> list[Any]:
     """Convert the values to a string type if SQLite3 cannot automatically interpret the type.
     SQLite 3 can interpret the following types: None, int, float, str, bytes.
     If the type is not one of these types, it will be converted to a string.
-    The exception is bool, which will be converted to an int."""
+    The exception is bool, which will be converted to an int.
+    """
     for i, value in enumerate(values):
         if isinstance(value, (str, int, float, bytes, type(None))):
             continue
