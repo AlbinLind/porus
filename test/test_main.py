73fb69620f865d25a372c01b90d747f001b523d0
Albin Lindqvist <albin.lindqvist@hotmail.se>
2024-02-28 15:47:18 +0100

    Adds documentation for main tests

--- test/test_main.py
+++ test/test_main.py
@@ -1,12 +1,17 @@
-from pydantic import ValidationError
+"""Tests the main functionality of the library, including select statement
+and creating and inserting table.
+"""
 import pytest
+from pydantic import ValidationError
+
 from porus.column import ColumnField
 from porus.engine import Engine
-from porus.table import Table
 from porus.main import User
+from porus.table import Table
 
 
 def test_create_table():
+    """Tests creating a table with the porus ORM."""
     class A(Table):
         id: int
         something: str
@@ -19,12 +24,14 @@ def test_create_table():
     _ = A(id="1", something="sdfo")
 
 
-def test_add_database():
+def test_create_engine():
+    """Tests setting up the engine."""
     engine = Engine(":memory:")
     engine.conn.execute("SELECT * FROM sqlite_master")
 
 
 def test_add_table():
+    """Tests adding a table to the database."""
     engine = Engine(":memory:")
     engine.push(User)
     res = engine.conn.execute("SELECT * FROM sqlite_master").fetchone()
@@ -32,6 +39,7 @@ def test_add_table():
 
 
 def test_add_single_row():
+    """Tests adding a single row to the database."""
     engine = Engine(":memory:")
     engine.push(User)
     usr = User(name="Hello")
@@ -41,6 +49,7 @@ def test_add_single_row():
 
 
 def test_add_multiple_rows():
+    """Tests adding multiple rows to the database."""
     engine = Engine(":memory:")
     engine.push(User)
     usr = User(name="Hello")
@@ -51,6 +60,7 @@ def test_add_multiple_rows():
 
 
 def test_inserting_returns_same_object():
+    """Tests that the insert method returns the same object that was inserted."""
     engine = Engine(":memory:")
     engine.push(User)
     usr = User(name="Hello")
@@ -59,6 +69,7 @@ def test_inserting_returns_same_object():
 
 
 def test_query_single_row():
+    """Tests querying a single row from the database."""
     engine = Engine(":memory:")
     engine.push(User)
     usr = User(name="Hello")
@@ -69,6 +80,7 @@ def test_query_single_row():
 
 
 def test_query_with_limit():
+    """Tests querying with a limit clause."""
     engine = Engine(":memory:")
     engine.push(User)
     usr = User(name="Hello")
@@ -81,6 +93,7 @@ def test_query_with_limit():
 
 
 def test_query_with_offset():
+    """Tests querying with an offset clause."""
     engine = Engine(":memory:")
     engine.push(User)
     usr = User(name="Hello")
@@ -93,6 +106,7 @@ def test_query_with_offset():
 
 
 def test_query_with_limit_and_offset():
+    """Tests querying with a limit and offset clause."""
     engine = Engine(":memory:")
     engine.push(User)
     usr = User(name="Hello")
@@ -106,6 +120,7 @@ def test_query_with_limit_and_offset():
 
 
 def test_query_with_where():
+    """Tests querying with a where clause."""
     engine = Engine(":memory:")
     engine.push(User)
     usr = User(name="Hello")
@@ -119,6 +134,7 @@ def test_query_with_where():
 
 
 def test_query_with_multiple_where():
+    """Tests querying with a where clause."""
     engine = Engine(":memory:")
     engine.push(User)
     usr = User(name="Hello")
@@ -137,6 +153,7 @@ def test_query_with_multiple_where():
 
 
 def test_column_query_function_sum():
+    """Tests the sum function in a query."""
     class A(Table):
         id: int = ColumnField(primary_key=True)
         number: int
@@ -153,6 +170,7 @@ def test_column_query_function_sum():
 
 
 def test_column_query_function_avg():
+    """Tests the avg function in a query."""
     class A(Table):
         id: int = ColumnField(primary_key=True)
         number: int
@@ -169,6 +187,7 @@ def test_column_query_function_avg():
 
 
 def test_column_query_function_max():
+    """Tests the max function in a query."""
     class A(Table):
         id: int = ColumnField(primary_key=True)
         number: int
@@ -185,6 +204,7 @@ def test_column_query_function_max():
 
 
 def test_column_query_function_min():
+    """Tests the min function in a query."""
     class A(Table):
         id: int = ColumnField(primary_key=True)
         number: int
@@ -201,6 +221,7 @@ def test_column_query_function_min():
 
 
 def test_column_query_function_count():
+    """Tests the count function in a query."""
     class A(Table):
         id: int = ColumnField(primary_key=True)
         number: int
@@ -217,6 +238,7 @@ def test_column_query_function_count():
 
 
 def test_order_by_query():
+    """Tests the order by clause in a query."""
     engine = Engine(":memory:")
 
     class A(Table):
@@ -239,6 +261,7 @@ def test_order_by_query():
 
 
 def test_order_by_query_desc():
+    """Tests the order by clause in a query with descending order."""
     engine = Engine(":memory:")
 
     class A(Table):
@@ -261,6 +284,7 @@ def test_order_by_query_desc():
 
 
 def test_group_by_query():
+    """Tests the group by clause in a query."""
     engine = Engine(":memory:")
 
     class A(Table):
@@ -283,6 +307,7 @@ def test_group_by_query():
 
 
 def test_ne_query():
+    """Tests the not equal operator in a query."""
     engine = Engine(":memory:")
     engine.push(User)
     usr = User(name="Hello")
@@ -296,7 +321,8 @@ def test_ne_query():
     assert res[1].name == usr3.name
 
 
-def test_IN_SQL_command():
+def test_IN_SQL_command():  # noqa: N802
+    """Tests the IN SQL command."""
     engine = Engine(":memory:")
     engine.push(User)
     usr = User(name="Hello")
@@ -310,7 +336,8 @@ def test_IN_SQL_command():
     assert res[1].name == usr3.name
 
 
-def test_COMPLEX_SQL_command():
+def test_COMPLEX_SQL_command():  # noqa: N802
+    """Tests the complex SQL command with IN and NOT IN."""
     engine = Engine(":memory:")
     engine.push(User)
     usr = User(name="Hello")
@@ -327,7 +354,8 @@ def test_COMPLEX_SQL_command():
     assert res[0].name == usr3.name
 
 
-def test_COMPLEX_SQL_command_2():
+def test_COMPLEX_SQL_command_2():  # noqa: N802
+    """Tests the complex SQL command with IN and NOT IN."""
     class A(Table):
         id: int = ColumnField(primary_key=True)
         name: str
@@ -347,7 +375,7 @@ def test_COMPLEX_SQL_command_2():
         .where(
             A.c.name["Name number 2", "Name number 3"]
             & (A.c.name != "Name number 2")
-            & (A.c.boolean == True)
+            & (A.c.boolean == True)  # noqa: E712
         )  # noqa: E712
         .all()
     )
@@ -358,6 +386,7 @@ def test_COMPLEX_SQL_command_2():
 
 
 def test_query_with_only_one_column():
+    """Tests querying with only one column."""
     engine = Engine(":memory:")
     engine.push(User)
     usr = User(name="Hello")
@@ -371,6 +400,7 @@ def test_query_with_only_one_column():
 
 
 def test_query_with_two_columns():
+    """Tests querying with two columns."""
     engine = Engine(":memory:")
     engine.push(User)
     usr = User(name="Hello")
