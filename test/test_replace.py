2095486c6489e34dd61bf58aeebd170bb6a1c167
Albin Lindqvist <albin.lindqvist@hotmail.se>
2024-03-14 10:44:06 +0100

    Fix error in test_replace code

--- test/test_replace.py
+++ test/test_replace.py
@@ -4,7 +4,7 @@ from porus.engine import Engine
 from porus.table import Table
 
 
-class TestTable(Table):
+class TableTest(Table):
     """A test table."""
 
     id: int = ColumnField(primary_key=True)
@@ -15,17 +15,17 @@ class TestTable(Table):
 def test_replacing_one():
     """Tests replacing one row in the table."""
     engine = Engine(":memory:")
-    engine.push(TestTable)
+    engine.push(TableTest)
 
-    obj_a = TestTable(column_one="Hello World!", column_two=42)
-    obj_b = TestTable(column_one="Goodbye World!", column_two=-123)
+    obj_a = TableTest(column_one="Hello World!", column_two=42)
+    obj_b = TableTest(column_one="Goodbye World!", column_two=-123)
 
     obj_a, obj_b = engine.insert([obj_a, obj_b])
     obj_a_inital_id = obj_a.id
     obj_a.column_one = "Once upon a time"
 
     obj_a = engine.replace(obj_a)
-    in_db_obj_a = engine.query(TestTable).where(TestTable.c.id == obj_a_inital_id).all()
+    in_db_obj_a = engine.query(TableTest).where(TableTest.c.id == obj_a_inital_id).all()
     assert in_db_obj_a[0].column_one == "Once upon a time"
     assert in_db_obj_a[0].column_two == 42
     assert in_db_obj_a[0].id == obj_a_inital_id
@@ -34,10 +34,10 @@ def test_replacing_one():
 def test_replacing_many():
     """Tests replacing many rows in the table."""
     engine = Engine(":memory:")
-    engine.push(TestTable)
+    engine.push(TableTest)
 
-    obj_a = TestTable(column_one="Hello World!", column_two=42)
-    obj_b = TestTable(column_one="Goodbye World!", column_two=-123)
+    obj_a = TableTest(column_one="Hello World!", column_two=42)
+    obj_b = TableTest(column_one="Goodbye World!", column_two=-123)
 
     obj_a, obj_b = engine.insert([obj_a, obj_b])
     obj_a_inital_id = obj_a.id
@@ -46,8 +46,8 @@ def test_replacing_many():
     obj_b.column_two = 123
 
     obj_a, obj_b = engine.replace(obj_a, obj_b)
-    in_db_obj_a = engine.query(TestTable).where(TestTable.c.id == obj_a_inital_id).all()
-    in_db_obj_b = engine.query(TestTable).where(TestTable.c.id == obj_b_inital_id).all()
+    in_db_obj_a = engine.query(TableTest).where(TableTest.c.id == obj_a_inital_id).all()
+    in_db_obj_b = engine.query(TableTest).where(TableTest.c.id == obj_b_inital_id).all()
     assert in_db_obj_a[0].column_one == "Once upon a time"
     assert in_db_obj_a[0].column_two == 42
     assert in_db_obj_a[0].id == obj_a_inital_id
